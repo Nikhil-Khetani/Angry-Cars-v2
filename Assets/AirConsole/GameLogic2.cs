24dc2bd5671ac9bba1c21e5de82844b04e4d8f61
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-03-11 03:11:59 +0000

    Added points and reset system

--- Assets/AirConsole/GameLogic2.cs
+++ Assets/AirConsole/GameLogic2.cs
@@ -20,12 +20,17 @@ public class GameLogic2 : MonoBehaviour {
 	private float prevFirstDistanceAlongPath;
 	private float FirstDistanceAlongPath;
 	private float playerDist;
-	public float maxDistanceBehind = 100;
+	[Range (0,50.0f)]
+	public float maxDistanceBehind =20.0f;
 	private Vector3 pathDirection;
 	private float pathLength;
 	private int firstPlayerLap;
 	private int prevFirstPlayerLap;
 	private int i=0;
+	private int alive_players;
+	private bool driving;
+	private Vector3 cornerStartPosition;
+	private Vector3 startPosition;
 
 	private List<Material> CarMats = new List<Material> ();
 	private Material CarMat;
@@ -33,8 +38,8 @@ public class GameLogic2 : MonoBehaviour {
 	private GameObject myPath;
 
 	public float CarVelocity=0.1f;
-	[Range (0, 10.0f)]
-	public float maxDistanceFromPath = 20.0f;
+	[Range (0, 50.0f)]
+	public float maxDistanceFromPath=20.0f;
 
 
 
@@ -50,9 +55,27 @@ public class GameLogic2 : MonoBehaviour {
 		for(i=0; i<9; i++){
 			CarMat = Resources.Load<Material>("Car"+ i.ToString());
 			CarMats.Add(CarMat);
-		}
-
+		}  	
 	}
+
+	void Start(){
+		StartCoroutine(restartCoroutine());
+	}
+
+	   
+    
+
+    IEnumerator restartCoroutine()
+    {
+        //Print the time of when the function is first called.
+        //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        yield return new WaitForSeconds(30);
+		//Debug.Log("End Coroutine at timestamp : " + Time.time);
+		prevFirstDistanceAlongPath = 0;
+		FirstDistanceAlongPath = 0;
+		restartAll(50);
+		
+    }
 
 	void OnReady(string code){
 		//Since people might be coming to the game from the AirConsole store once the game is live, 
@@ -78,8 +101,8 @@ public class GameLogic2 : MonoBehaviour {
 
 		//Instantiate player prefab, store device id + player script in a dictionary
 		GameObject newPlayer = Instantiate (playerPrefab, transform.position+= new Vector3(2*deviceID, 0,-2*deviceID), transform.rotation) as GameObject;
-		newPlayer.GetComponent<Renderer>().material = CarMats[deviceID];
-		newPlayer.GetComponent<Player_Movement>().initialise(CarVelocity);
+
+		newPlayer.GetComponent<Player_Movement>().initialise(CarVelocity, CarMats[deviceID], driving);
 		playerObjects.Add(deviceID, newPlayer);
 		players.Add(deviceID, newPlayer.GetComponent<Player_Movement>());
 		
@@ -92,9 +115,7 @@ public class GameLogic2 : MonoBehaviour {
 	void OnMessage (int from, JToken data){
 		Debug.Log ("message: " + data);
 
-		//When I get a message, I check if it's from any of the devices stored in my device Id dictionary
 		if (players.ContainsKey (from) && data["move"] != null) {
-			//I forward the command to the relevant player script, assigned by device ID
 			players [from].Move(data["move"].ToString());
 		}
 	}
@@ -124,10 +145,22 @@ public class GameLogic2 : MonoBehaviour {
 			}
 
 			else if(prevFirstDistanceAlongPath - playerDist>maxDistanceBehind){
-				player.Value.explode();
+				explodePlayer(player.Value);
 			}
-
+			if(player.Value.getPlayerDistanceToPath()>maxDistanceFromPath){
+				explodePlayer(player.Value);
+			}
 		}
+
+		if (alive_players<2){
+			foreach(var player in players){
+				if(player.Value.checkIfExploded()==false){
+					player.Value.changePoints(2);
+				}
+			}
+			restartAll(prevFirstDistanceAlongPath-20);
+		}
+
 		pathDirection = pathCreator.path.GetDirectionAtDistance(prevFirstDistanceAlongPath);
 		camera_offset = -20*pathDirection + camera_y_offset;
 		camera_pos = pathCreator.path.GetPointAtDistance(prevFirstDistanceAlongPath) + camera_offset;
@@ -137,4 +170,59 @@ public class GameLogic2 : MonoBehaviour {
 
 
 	}
+
+	void explodePlayer(Player_Movement player){
+		if(alive_players == players.Count){
+			player.changePoints(-2);
+		}
+		if(players.Count>4){
+			if(alive_players == players.Count-1){
+				player.changePoints(-1);
+			}
+			else if(alive_players == 2){
+				player.changePoints(1);
+			}
+		}
+		alive_players-=1;
+
+	}
+
+	void restartAll(float dist){
+		alive_players=0;
+		foreach (var player in players)
+		{
+			restart(dist-5, player.Key , player.Value);
+			alive_players+=1;
+		}
+		pathDirection = pathCreator.path.GetDirectionAtDistance(dist-10);
+		camera_offset = -20*pathDirection + camera_y_offset;
+		camera_pos = pathCreator.path.GetPointAtDistance(dist-10) + camera_offset;
+
+		Camera.main.transform.rotation = Quaternion.LookRotation(pathDirection+ new Vector3(0,-1,0));
+		Camera.main.transform.position = camera_pos;
+		StartCoroutine(GoCoroutine());
+		
+	}
+
+	void restart(float pathPosition, int position, Player_Movement player){
+		//Debug.Log("Restart from GameLogic");
+		cornerStartPosition = pathCreator.path.GetPointAtDistance(pathPosition) + Quaternion.LookRotation(pathCreator.path.GetDirectionAtDistance(pathPosition))* new Vector3(-4,5,10);
+		position-=1;
+		driving = false;
+		startPosition = cornerStartPosition + new Vector3(2.5f*(position%4),0,8.5f*(position-(position%4)) );
+		player.restartCar(startPosition, pathCreator.path.GetDirectionAtDistance(pathPosition) );		
+	}
+
+	IEnumerator GoCoroutine()
+    {
+        //Print the time of when the function is first called.
+        //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        yield return new WaitForSeconds(3);
+		//Debug.Log("End Coroutine at timestamp : " + Time.time);
+		foreach (var player in players)
+		{
+			player.Value.Go();
+			Debug.Log("Go from player");
+		}
+    }
 }
--- Assets/AirConsole/Player_Movement.cs
+++ Assets/AirConsole/Player_Movement.cs
@@ -17,18 +17,35 @@ public class Player_Movement : MonoBehaviour
     private float prevDist;
     private float newDist;
     private int lap;
-    private bool state;
+    public bool driving;
+    private bool exploded;
+
+    public float centerOfMassDepth;
+    private Material alive_material;
+    public Material exploded_material;
     //1=normal, 2=frozen, 3=exploded
     // Start is called before the first frame update
+
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    public int points=0;
+    
+
     void Start()
     {
       pathCreator = GameObject.Find("Path").GetComponent<PathCreator>();
       //rb = GetComponent<Rigidbody>();   
-      rb.centerOfMass += new Vector3(0,-0.4f,0);
+      rb.centerOfMass += new Vector3(0, centerOfMassDepth,0);
       lap=0;
+      
     }
-    public void initialise(float velocity){
+
+    public void initialise(float velocity, Material mat, bool driv){
         vel = velocity;
+        alive_material = mat;
+        driving = driv;
+        this.GetComponent<Renderer>().material=alive_material;
     }
     public void Move(string data){
         switch(data){
@@ -53,27 +70,52 @@ public class Player_Movement : MonoBehaviour
     {
         
     }
+
+    public void restartCar(Vector3 newPosition, Vector3 newOrientation){
+        //Debug.Log("Restart this car");
+        driving = false;
+        exploded = false;
+        this.GetComponent<Renderer>().material = alive_material;
+        transform.position = newPosition;
+        transform.rotation = Quaternion.LookRotation(newOrientation);
+        rb.velocity = new Vector3(0,0,0);
+    }
+
+    public void Go(){
+        Debug.Log("Go from player");
+        driving=true;
+    }
+
     void FixedUpdate(){
-        if(left==1 && right==1){
-            rb.AddForce(transform.forward*-0.1f);
+        float steering;
+        float motor;
+        if (driving){
+        motor = maxMotorTorque * vel;
+        steering = maxSteeringAngle * (right-left);
         }
         else{
-        rb.AddForce(transform.forward * vel);
-        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
-        localVelocity.x = 0;
-        rb.velocity= transform.TransformDirection(localVelocity);
+            motor = 0;
+            steering = 0;
         }
-
-
-        transform.Rotate(new Vector3 (0, (right-left), 0));
-        if (transform.position.y<-100){
-            transform.position = new Vector3(10,10,20);
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+                
+            }
+        
         }
     }
 
-
     public void explode(){
         Debug.Log("Exploded");
+        driving=false;
+        exploded = true;
+        this.GetComponent<Renderer>().material = exploded_material;
     }
 
     public float getPlayerDistanceAlongPath(){
@@ -87,8 +129,19 @@ public class Player_Movement : MonoBehaviour
         prevDist = newDist;
         return newDist;
     }
+    public float getPlayerDistanceToPath(){
+        return Vector3.Distance(transform.position,pathCreator.path.GetClosestPointOnPath(transform.position));
+    }
+    public void changePoints(int change){
+        points+=change;
+        if(points<0){
+            points=0;
+        }
+    }
 
-
+    public bool checkIfExploded(){
+        return driving;
+    }
 
     public int getLap(){
         return lap;
@@ -103,4 +156,11 @@ public class Player_Movement : MonoBehaviour
         return transform.position.z;
     }
 
+}
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
 }
